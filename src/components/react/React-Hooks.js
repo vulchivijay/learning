35d5dbd0ba55253da40974065cdc32eb0b70030d
Vijaya Kumar V <17252359+vulchivijay@users.noreply.github.com>
2021-08-03 18:06:46 +0530

    Update React-Hooks.js

--- src/components/react/React-Hooks.js
+++ src/components/react/React-Hooks.js
@@ -1,132 +1,91 @@
 import React, {
   useState,
   useEffect,
-  useContext,
   useReducer,
+  useMemo,
   useRef,
-  useMemo
+  useCallback,
+  createContext,
+  useContext
 } from 'react';
 import './style.css';
 
-const themes = {
-  light: {
-    foreground: '#000000',
-    background: '#eeeeee'
-  },
-  dark: {
-    foreground: '#ffffff',
-    background: '#222222'
-  }
-};
+const initialState = { count: 0 };
+const functionsSet = new Set();
 
-const ThemeContext = React.createContext(themes.light);
-const initialState = { number: 0 };
+export const userDetailContext = createContext(null);
 
 export default function App() {
   const [count, setCount] = useState(0);
-  const [fruit, setFruit] = useState('banana');
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [number, setNumber] = useState(1);
+  const [inc, setInc] = useState(0);
+  const factorial = useMemo(() => factorialOf(number), [number]);
+  const [userDetails] = useState({
+    name: 'Mayank',
+    age: 30
+  });
 
   useEffect(() => {
-    console.log('use effect triggered!');
-    setFruit('apple');
+    setCount(count + 1);
   }, []);
 
-  console.log('render function called');
+  const onChange = event => {
+    setNumber(Number(event.target.value));
+  };
+
+  const onClick = () => setInc(i => i + 1);
 
   return (
     <div>
-      <p>Basic Hooks</p>
-      <ol>
-        <li>useState initial state: {count}</li>
-        <li>
-          useEffect initial <b>banana</b> after useeffect: <b>{fruit}</b>
-          <p>
-            useEffect runs asynchronously and after a render is painted to the
-            screen.
-          </p>
-          <p>
-            So that looks like: You cause a render somehow (change state, or the
-            parent re-renders) React renders your component (calls it) The
-            screen is visually updated THEN useEffect runs
-          </p>
-        </li>
-        <li>
-          useContext
-          <ThemeContext.Provider value={themes.dark}>
-            <Toolbar />
-          </ThemeContext.Provider>
-        </li>
-      </ol>
-      <p>Additional Hooks</p>
-      <ol>
-        <li>
-          useReducer: <Counter />
-        </li>
-        <li>useCallback: </li>
-        <li>
-          useRef: <TextInputWithFocusButton />
-        </li>
-        <li>
-          useMemo: <CalculateFactorialWithOutUseMemo />{' '}
-          <CalculateFactorialUseMemo />
-        </li>
-        <li>useImperativeHandle</li>
-        <li>
-          useLayoutEffect
-          <p>
-            useLayoutEffect, on the other hand, runs synchronously after a
-            render but before the screen is updated. That goes: You cause a
-            render somehow (change state, or the parent re-renders) React
-            renders your component (calls it) useLayoutEffect runs, and React
-            waits for it to finish. The screen is visually updated
-          </p>
-        </li>
-        <li>useDebugValue</li>
-      </ol>
+      <p>1. Count: {count} updating from useEffect hook/method.</p>
+      <p>2. Count: {state.count} update from useReducer hook/method.</p>
+      <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+      {'  '}
+      <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+      <p>3. Factorial of updating from useMemo hook/method.</p>
+      <input type="number" value={number} onChange={onChange} /> is {factorial}{' '}
+      {'  '}
+      <button onClick={onClick}>Re-render</button>
+      <p>4. Count updating from useCallback hook.</p>
+      <CounterCallback />
+      <p>5. focus the input field when user click focus the input button</p>
+      <TextInputWithFocusButton />
+      <p>6. updating from useContact hook/method.</p>
+      <div>
+        <p>Parent Component</p>
+        <p>User Name: {userDetails.name}</p>
+        <p>User Age: {userDetails.age}</p>
+        <userDetailContext.Provider value={userDetails}>
+          <ChildComponent />
+        </userDetailContext.Provider>
+      </div>
+      <p>
+        7. useLayoutEffect - useLayoutEffect, on the other hand, runs
+        synchronously after a render but before the screen is updated. That
+        goes: You cause a render somehow (change state, or the parent
+        re-renders) React renders your component (calls it) useLayoutEffect
+        runs, and React waits for it to finish. The screen is visually updated
+      </p>
     </div>
-  );
-}
-
-function Toolbar(props) {
-  return (
-    <div>
-      <ThemedButton />
-    </div>
-  );
-}
-
-function ThemedButton() {
-  const theme = useContext(ThemeContext);
-  return (
-    <button style={{ background: theme.background, color: theme.foreground }}>
-      I am styled by theme context!
-    </button>
   );
 }
 
 function reducer(state, action) {
-  console.log(state);
   switch (action.type) {
     case 'increment':
-      return { number: state.number + 1 };
+      return { count: state.count + 1 };
     case 'decrement':
-      return { number: state.number - 1 };
+      return { count: state.count - 1 };
     default:
       throw new Error();
   }
 }
 
-function Counter() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return (
-    <>
-      {state.number}{' '}
-      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
-      {'  '}
-      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
-    </>
-  );
-}
+const factorialOf = n => {
+  console.log('factorialOf(n) called!');
+  return n <= 0 ? 1 : n * factorialOf(n - 1);
+};
 
 function TextInputWithFocusButton() {
   const inputEl = useRef(null);
@@ -142,81 +101,93 @@ function TextInputWithFocusButton() {
   );
 }
 
-function CalculateFactorialWithOutUseMemo() {
-  const [number, setNumber] = useState(1);
-  const [inc, setInc] = useState(0);
-
-  const factorial = factorialOf(number);
-
-  const onChange = event => {
-    setNumber(Number(event.target.value));
+function ChildComponent() {
+  const padding = {
+    padding: '5px',
+    backgroundColor: 'orange'
   };
-
-  const onClick = () => setInc(i => i + 1);
-
   return (
-    <div>
-      <h1>Calculate Factorial without useMemo hook</h1>
-      Factorial of
-      <input type="number" value={number} onChange={onChange} />
-      is {factorial}
-      <button onClick={onClick}>Re-render</button>
-      <p>
-        Every time you change the input value, the factorial is calculated
-        factorialOf(n) and 'factorialOf(n) called!' is logged to console.
-      </p>
-      <p>
-        On the other side, each time you click Re-render button, inc state value
-        is updated. Updating inc state value triggers CalculateFactorial
-        re-rendering. But, as a secondary effect, during re-rendering the
-        factorial is recalculated again — 'factorialOf(n) called!' is logged to
-        console.
-      </p>
+    <div style={padding}>
+      <p>Child Component</p>
+      <ChildChildComponent />
     </div>
   );
 }
 
-function CalculateFactorialUseMemo() {
-  const [number, setNumber] = useState(1);
-  const [inc, setInc] = useState(0);
-
-  const factorial = useMemo(() => factorialOf(number), [number]);
-
-  const onChange = event => {
-    setNumber(Number(event.target.value));
+function ChildChildComponent() {
+  const contextData = useContext(userDetailContext);
+  const padding = {
+    padding: '5px',
+    backgroundColor: 'purple',
+    color: '#fff'
   };
-  const onClick = () => setInc(i => i + 1);
-
   return (
-    <div>
-      <h1>Calculate Factorial with useMemo hook</h1>
-      <p>
-        useMemo() is a built-in React hook that accepts 2 arguments — a function
-        compute that computes a result and the depedencies array:
-      </p>
-      Factorial of
-      <input type="number" value={number} onChange={onChange} />
-      is {factorial}
-      <button onClick={onClick}>Re-render</button>
-      <p>
-        Every time you change the value of the number, 'factorialOf(n) called!'
-        is logged to console. That’s expected.
-      </p>
-      <p>
-        However, if you click Re-render button, 'factorialOf(n) called!' isn’t
-        logged to console because useMemo(() factorialOf(number), [number])
-        returns the memoized factorial calculation. Great!
-      </p>
+    <div style={padding}>
+      <p>Child Child Component</p>
+      <p>About page Child component sub Child component</p>
+      <p>User Name: {contextData.name}</p>
+      <p>User Age: {contextData.age}</p>
     </div>
   );
 }
 
-const factorialOf = n => {
-  console.log('factorialOf(n) called!');
-  return n <= 0 ? 1 : n * factorialOf(n - 1);
+const CounterCallback = () => {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  // increment is re-created on each render
+  // const increment = () => {
+  //   setCount(count + 1);
+  // };
+
+  // decrement is re-created on each render
+  // const decrement = () => {
+  //   setCount(count - 1);
+  // };
+
+  // incrementOtherCounter is re-created on each render
+  // const incrementOtherCounter = () => {
+  //   setCount2(count2 + 1);
+  // };
+
+  // incrementCallback is the same function object
+  const increment = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
+
+  // decrement is the same function object
+  const decrement = useCallback(() => {
+    setCount(count - 1);
+  }, [count]);
+
+  // incrementOtherCounter is the same function object
+  const incrementOtherCounter = useCallback(() => {
+    setCount2(count2 + 1);
+  }, [count2]);
+
+  functionsSet.add(increment);
+  functionsSet.add(decrement);
+  functionsSet.add(incrementOtherCounter);
+
+  console.log(functionsSet);
+
+  return (
+    <>
+      Count: {count} <button onClick={increment}>+</button>{' '}
+      <button onClick={decrement}>-</button>{' '}
+      <button onClick={incrementOtherCounter}>increment Other Counter</button>
+      <p>
+        This is pretty simple, we have two state variables that are holding
+        numbers and three functions to change our state. However, the problem
+        here is that every time the Counter component is re-rendered, all three
+        functions, increment , decrement , and incrementOtherCounter are all
+        recreated!
+      </p>
+      <p>
+        We can see that by using a Set and adding the function to the Set each
+        time the function runs. Why Set? It only stores unique elements, or in
+        our case, uniquely instantiated functions.
+      </p>
+    </>
+  );
 };
-
-// function factorialOf(n) {
-//   console.log('factorialOf(n) called!');
-//   return n <= 0 ? 1 : n * factorialOf(n - 1);
-// }
