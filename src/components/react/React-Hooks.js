31877e62b9f3c9db22be6a58abebef1ba8b3accb
Vijaya Kumar V <17252359+vulchivijay@users.noreply.github.com>
2021-08-03 15:29:14 +0530

    Update and rename Hooks-LifeCycles.js to React-Hooks.js

--- src/components/react/React-Hooks.js
+++ src/components/react/React-Hooks.js
@@ -3,8 +3,10 @@ import React, {
   useEffect,
   useContext,
   useReducer,
-  useRef
+  useRef,
+  useMemo
 } from 'react';
+import './style.css';
 
 const themes = {
   light: {
@@ -20,7 +22,7 @@ const themes = {
 const ThemeContext = React.createContext(themes.light);
 const initialState = { number: 0 };
 
-export default function ReactFunLifeCycles() {
+export default function App() {
   const [count, setCount] = useState(0);
   const [fruit, setFruit] = useState('banana');
 
@@ -64,7 +66,10 @@ export default function ReactFunLifeCycles() {
         <li>
           useRef: <TextInputWithFocusButton />
         </li>
-        <li>useMemo: </li>
+        <li>
+          useMemo: <CalculateFactorialWithOutUseMemo />{' '}
+          <CalculateFactorialUseMemo />
+        </li>
         <li>useImperativeHandle</li>
         <li>
           useLayoutEffect
@@ -78,7 +83,6 @@ export default function ReactFunLifeCycles() {
         </li>
         <li>useDebugValue</li>
       </ol>
-      <p></p>
     </div>
   );
 }
@@ -137,3 +141,82 @@ function TextInputWithFocusButton() {
     </>
   );
 }
+
+function CalculateFactorialWithOutUseMemo() {
+  const [number, setNumber] = useState(1);
+  const [inc, setInc] = useState(0);
+
+  const factorial = factorialOf(number);
+
+  const onChange = event => {
+    setNumber(Number(event.target.value));
+  };
+
+  const onClick = () => setInc(i => i + 1);
+
+  return (
+    <div>
+      <h1>Calculate Factorial without useMemo hook</h1>
+      Factorial of
+      <input type="number" value={number} onChange={onChange} />
+      is {factorial}
+      <button onClick={onClick}>Re-render</button>
+      <p>
+        Every time you change the input value, the factorial is calculated
+        factorialOf(n) and 'factorialOf(n) called!' is logged to console.
+      </p>
+      <p>
+        On the other side, each time you click Re-render button, inc state value
+        is updated. Updating inc state value triggers CalculateFactorial
+        re-rendering. But, as a secondary effect, during re-rendering the
+        factorial is recalculated again — 'factorialOf(n) called!' is logged to
+        console.
+      </p>
+    </div>
+  );
+}
+
+function CalculateFactorialUseMemo() {
+  const [number, setNumber] = useState(1);
+  const [inc, setInc] = useState(0);
+
+  const factorial = useMemo(() => factorialOf(number), [number]);
+
+  const onChange = event => {
+    setNumber(Number(event.target.value));
+  };
+  const onClick = () => setInc(i => i + 1);
+
+  return (
+    <div>
+      <h1>Calculate Factorial with useMemo hook</h1>
+      <p>
+        useMemo() is a built-in React hook that accepts 2 arguments — a function
+        compute that computes a result and the depedencies array:
+      </p>
+      Factorial of
+      <input type="number" value={number} onChange={onChange} />
+      is {factorial}
+      <button onClick={onClick}>Re-render</button>
+      <p>
+        Every time you change the value of the number, 'factorialOf(n) called!'
+        is logged to console. That’s expected.
+      </p>
+      <p>
+        However, if you click Re-render button, 'factorialOf(n) called!' isn’t
+        logged to console because useMemo(() factorialOf(number), [number])
+        returns the memoized factorial calculation. Great!
+      </p>
+    </div>
+  );
+}
+
+const factorialOf = n => {
+  console.log('factorialOf(n) called!');
+  return n <= 0 ? 1 : n * factorialOf(n - 1);
+};
+
+// function factorialOf(n) {
+//   console.log('factorialOf(n) called!');
+//   return n <= 0 ? 1 : n * factorialOf(n - 1);
+// }
